eef57adcbf3e66032d71a7156ebfce1321eaa671
Ilya Zadorozhny <ilya.zadorozhny@gmail.com>
2022-03-15 00:47:58 +0300

    jest config

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,18 @@
-const { pathsToModuleNameMapper } = require('ts-jest/utils')
-const { paths } = require('./tsconfig.json').compilerOptions
+// const { pathsToModuleNameMapper } = require('ts-jest')
+// const { paths } = require('./tsconfig.json').compilerOptions
 
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 
 const config = {
+  rootDir: '.',
   preset: 'jest-preset-angular',
-  moduleNameMapper: pathsToModuleNameMapper(paths, { prefix: '<rootDir>' }),
+  // moduleNameMapper: pathsToModuleNameMapper(paths, { prefix: '<rootDir>' }),
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.ts',
+    '\\.(html)$': '<rootDir>/__mocks__/fileMock.ts',
+    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.ts',
+  },
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
 }
 module.exports = config
